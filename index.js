cc8b0fcde3f9e8ebd3ea272217d9c6df361e1f08
Riyan Satria <34812231+haloriyan@users.noreply.github.com>
2026-05-13 20:42:12 +0700

    qqwsdqwfqfd

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ const { sessions, createSession, destroySession, restoreSessions } = require('./
 const app = express();
 app.use(express.json());
 
-const PORT = process.env.PORT || 6969;
+const PORT = process.env.PORT || 6942;
 const BASE_URL = (process.env.BASE_URL || `http://localhost:${PORT}`).replace(/\/$/, '');
 
 // POST /connect
