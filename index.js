2c865ca29b6d2e65fb440af48ae6a8bfbcd7c2b2
Riyan Satria <riyan.satria.619@icloud.com>
2026-05-13 20:29:57 +0700

    eruhfiaugiuxwe083f

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ const { sessions, createSession, destroySession, restoreSessions } = require('./
 const app = express();
 app.use(express.json());
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 6969;
 const BASE_URL = (process.env.BASE_URL || `http://localhost:${PORT}`).replace(/\/$/, '');
 
 // POST /connect
